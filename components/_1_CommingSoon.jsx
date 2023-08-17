18b1209da4bb8134ef590b9f0c9963d2cbc7dd9c
love-to-code108 <love.to.code108@gmail.com>
2023-08-18 01:14:06 +0530

    made an okay looking starting animation

--- components/_1_CommingSoon.jsx
+++ components/_1_CommingSoon.jsx
@@ -2,23 +2,41 @@ import "../src/index.css"
 
 // ASSETS
 import commingSoonLogo from "../src/assets/svg/tecxaroLogo.svg"
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { Timeline } from "gsap/gsap-core";
 
 
 
 export const CommingSoon = () => {
 
+    gsap.registerPlugin(Timeline);
+
+    useEffect(() => {
+        const t1 = gsap.timeline();
+        t1.fromTo("#CommingSoon",{
+
+            y:100,
+        },{
+            y:0,
+            duration:1,
+            ease:"elastic",
+            delay:4,
+        })
+
+    },[])
 
 
     return(
-        <div className="commingSoon flex flex-col justify-center items-center px-4">
+        <div id="CommingSoon_1_" className="commingSoon flex flex-col justify-center items-center px-4">
 
 
             <div>
                 <img src={commingSoonLogo} alt="" />
             </div>
 
-            <div className=" font-poppins text-3xl font-black bg-gradient-to-r from-[#FF4536] to-[#FF8F4D]
-            px-2 text-white absolute bottom-4 pt-1
+            <div id="CommingSoon" className=" font-poppins text-3xl font-black bg-gradient-to-r from-[#FF4536] to-[#FF8F4D]
+            px-2 text-white fixed bottom-4 pt-1
             sm:text-4xl
             md:text-5xl 
             lg:text-6xl
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
 
 //IMPORTING ALL THE COMPONENTS 
 import { CommingSoon } from '../components/_1_CommingSoon'
+import { StartingAnimations } from '../components/_0-1_StartingAnimations'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+    <StartingAnimations/>
     <CommingSoon/>
   </React.StrictMode>,
 )
